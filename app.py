764c35b430d287c97a7c3025f700059de77eacc4
Anna Bowers <eenabow@gmail.com>
2021-02-03 19:45:22 -0500

    Adding code for tobs app

--- app.py
+++ app.py
@@ -40,8 +40,7 @@ def welcome():
         f"Available api routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end><br/>"
         )
@@ -66,4 +65,10 @@ def welcome():
 
         # return jsonify(all_stations)
 
+# @app.route("/api/v1.0/tobs<br/>")
+# def tobs():
+#         return jsonify(Most_active_station)
+        
 
+@app.route("/api/v1.0/<start><br/>")
+def 
